3934a5e938fef6f6e5f8fcc5b0edb90a9731e794
Gery <jery.vasileva@gmail.com>
2026-03-23 19:34:04 +0200

    Stiga tolkoz

--- php/Login.php
+++ php/Login.php
@@ -1,12 +1,12 @@
 <?php
-// Казваме на браузъра, че отговорът ще бъде само JSON
+
 header('Content-Type: application/json');
 
-// Взимаме данните от POST заявката
+
 $email = isset($_POST['email']) ? $_POST['email'] : '';
 $passowrd = isset($_POST['password']) ? $_POST['password'] : '';
 
-// Свързваме се с базата данни
+
 $conn = new mysqli('localhost', 'root', '', 'winepainting');
 
 if ($conn->connect_error) {
@@ -14,7 +14,7 @@ if ($conn->connect_error) {
     exit;
 }
 
-// 1. Поправена заявка: Търсим по имейл (колоните са id, email, password1)
+
 $log = $conn->prepare("SELECT id, email, password1 FROM users WHERE email = ?");
 
 if ($log) {
@@ -25,13 +25,11 @@ if ($log) {
     if ($get->num_rows > 0) {
         $data = $get->fetch_assoc();
         
-        // 2. Сравняваме паролата с колоната password1
+        
         if ($data['password1'] === $passowrd) {
-            // 3. Поправени сесии: start и SESSION
             session_start();
             $_SESSION['id'] = $data['id'];
             
-            // Връщаме Success статус в JSON формат
             echo json_encode(array("status" => "Success", "id" => $data['id']));
         } else {
             echo json_encode(array("status" => "Error", "message" => "Wrong password"));
